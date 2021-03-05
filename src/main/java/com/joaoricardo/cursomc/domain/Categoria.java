c8c3fc53e5bf9c70798ffb0408fb5bc46224557c
Joao Ricardo <jjrfbastos@gmail.com>
2021-03-05 08:51:50 -0300

    Produto e associação muitos para muitos com categoria

--- src/main/java/com/joaoricardo/cursomc/domain/Categoria.java
+++ src/main/java/com/joaoricardo/cursomc/domain/Categoria.java
@@ -1,11 +1,16 @@
 package com.joaoricardo.cursomc.domain;
 
+//Associação Muitos para muitso uma categoria tem varios produtos
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +21,9 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
 	public Categoria() {
 		
 	}
@@ -24,6 +32,13 @@ public class Categoria implements Serializable {
 		super();
 		this.id = id;
 		this.nome = nome;
+	}
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	public Integer getId() {
@@ -66,6 +81,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+
+
 	
 	
 
---
+++ src/main/java/com/joaoricardo/cursomc/domain/Produto.java
@@ -0,0 +1,108 @@
+package com.joaoricardo.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+
+// Mapeamento da classe produto com uma entidade do JPA
+@Entity
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private double preco;
+	
+	//Um produto tem uma ou mais categoria na associação
+	//Criando relacionamento muitos para muitos entre categoria e produtos com JPA
+	
+	@ManyToMany
+	//define a tabela no banco que vai fazer o relacionamento
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn(name = "produto_id"),//produto chave estrangeira
+			inverseJoinColumns = @JoinColumn(name = "categoria_id")
+			
+			) 
+	private List<Categoria> categorias = new ArrayList<>();
+	
+	//Criação do construtor vazio que é um metodo com mesmo nome da classe
+	public Produto(){
+	}
+	//Criação do construtor com argumentos que é um metodo com mesmo nome da classe
+
+	public Produto(Integer id, String nome, double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
