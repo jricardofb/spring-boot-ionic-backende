4d01bec17cbaedb345eb0a2516fea36f69b71a35
Joao Ricardo <jjrfbastos@gmail.com>
2021-05-11 06:11:35 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/joaoricardo/cursomc/domain/ItemPedido.java
+++ src/main/java/com/joaoricardo/cursomc/domain/ItemPedido.java
@@ -32,6 +32,7 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
+	// framework não utiliza o construtor como argumento ele utiliza o método Set
 
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
@@ -42,9 +43,18 @@ public class ItemPedido implements Serializable {
 		return id.getPedido();
 	}
 	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
 	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
 	
 	public ItemPedidoPK getId() {
 		return id;
--- src/main/java/com/joaoricardo/cursomc/services/PedidoService.java
+++ src/main/java/com/joaoricardo/cursomc/services/PedidoService.java
@@ -1,24 +1,62 @@
 package com.joaoricardo.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.joaoricardo.cursomc.domain.ItemPedido;
+import com.joaoricardo.cursomc.domain.PagamentoComBoleto;
 import com.joaoricardo.cursomc.domain.Pedido;
+import com.joaoricardo.cursomc.domain.enums.EstadoPagamento;
+import com.joaoricardo.cursomc.repositories.ItemPedidoRepository;
+import com.joaoricardo.cursomc.repositories.PagamentoRepository;
 import com.joaoricardo.cursomc.repositories.PedidoRepository;
+import com.joaoricardo.cursomc.repositories.ProdutoRepository;
 import com.joaoricardo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFoundException(
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoRepository.findById(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+	}
 }
